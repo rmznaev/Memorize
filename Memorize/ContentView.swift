b54b254f20fecce7fb1669a49bc949fafcb5bd58
Ramazan Abdullayev <ramazan.aev@gmail.com>
2022-11-01 16:13:24 +0100

    added more functionalities as adding and removing the cards

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -10,15 +10,46 @@ import CoreData
 
 struct ContentView: View {
     var emojis = ["⛴", "✈️", "🚂", "🚜", "🚗", "🚕", "🚙", "🚌", "🚎", "🏎", "🚒", "🚓", "🚑", "🚐", "🛻", "🚚", "🚛", "🛺", "🚍", "🚔", "🚖", "🚠", "🚡", "🚃"]
+    @State var emojiCount = 6
     
     var body: some View {
-        HStack {
-            ForEach(emojis[0..<5], id: \.self) { emoji in
-                CardView(content: emoji)
+        VStack {
+            HStack {
+                ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                    CardView(content: emoji)
+                }
             }
+            Spacer()
+            HStack {
+                remove
+                Spacer()
+                add
+            }
+            .font(.largeTitle)
+            .padding(.horizontal)
         }
         .padding(.horizontal)
         .foregroundColor(.red)
+    }
+    
+    var remove: some View {
+        Button {
+            if emojiCount > 1 {
+                emojiCount -= 1
+            }
+        } label: {
+            Image(systemName: "minus.circle")
+        }
+    }
+    
+    var add: some View {
+        Button {
+            if emojiCount < emojis.count {
+                emojiCount += 1
+            }
+        } label: {
+            Image(systemName: "plus.circle")
+        }
     }
 }
 
@@ -35,7 +66,7 @@ struct CardView: View {
                     .fill()
                     .foregroundColor(.white)
                 shape
-                    .stroke(lineWidth: 3)
+                    .stroke(lineWidth: 2)
                 Text(content)
                     .font(.largeTitle)
             } else {
