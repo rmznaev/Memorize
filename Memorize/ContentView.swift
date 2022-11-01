331497c64068b5f6eaa1797502b00cb8e24532c2
Ramazan Abdullayev <ramazan.aev@gmail.com>
2022-11-01 15:14:42 +0100

    added more cards and cleaned the code

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,50 +9,45 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
+    var emojis = ["⛴", "✈️", "🚂", "🚜", "🚗", "🚕", "🚙", "🚌", "🚎", "🏎", "🚒", "🚓", "🚑", "🚐", "🛻", "🚚", "🚛", "🛺", "🚍", "🚔", "🚖", "🚠", "🚡", "🚃"]
+    
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 20)
-                .stroke(lineWidth: 3)
-            Text("Hello SwiftUI!")
+        HStack {
+            ForEach(emojis[0..<5], id: \.self) { emoji in
+                CardView(content: emoji)
+            }
         }
         .padding(.horizontal)
         .foregroundColor(.red)
     }
 }
 
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+struct CardView: View {
+    var content: String = ""
+    @State var isFaceUp: Bool = true
+    
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            
+            if isFaceUp {
+                shape
+                    .fill()
+                    .foregroundColor(.white)
+                shape
+                    .stroke(lineWidth: 3)
+                Text(content)
+                    .font(.largeTitle)
+            } else {
+                shape
+                    .fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
+    }
+}
 
 
 
@@ -63,5 +58,8 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.light)
+        ContentView()
+            .preferredColorScheme(.dark)
     }
 }
